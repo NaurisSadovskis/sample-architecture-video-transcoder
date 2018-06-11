3c2b4094177b43439c4c3c416ad138b22364582a
Nauris Sadovskis <nauris.sadovskis@crowdcube.com>
2018-06-11 10:03:53 +0200

    Merge docker-compose files

--- init.sh
+++ init.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 ENVIRONMENT_CONFIG=".env"
-MINIO_COMPOSE_FILE="docker-compose-minio.yml"
 
 echo "Starting Minio container..."
-docker-compose -f $MINIO_COMPOSE_FILE up -d 
+docker-compose up -d 
 echo "Sleeping for 5 seconds..."
 sleep 5 # time to intialise
 
 if [ ! -f $ENVIRONMENT_CONFIG ]; then
     echo "Writing Minio secrets to $ENVIRONMENT_CONFIG file..."
-    docker-compose -f $MINIO_COMPOSE_FILE logs minio | grep AccessKey | sed 's/.*: /MINIO_ACCESS_KEY=/' >> $ENVIRONMENT_CONFIG
-    docker-compose -f $MINIO_COMPOSE_FILE logs minio | grep SecretKey | sed 's/.*: /MINIO_SECRET_KEY=/' >> $ENVIRONMENT_CONFIG
+    docker-compose logs minio | grep AccessKey | sed 's/.*: /MINIO_ACCESS_KEY=/' >> $ENVIRONMENT_CONFIG
+    docker-compose logs minio | grep SecretKey | sed 's/.*: /MINIO_SECRET_KEY=/' >> $ENVIRONMENT_CONFIG
     echo "Done!"
 fi
 
